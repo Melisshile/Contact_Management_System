713428f67f493456f8700b4d366bd5b366171242
Melisshile <hilairentibashima@gmail.com>
2024-07-15 14:25:35 +0200

    softDev

---
+++ Contact_Management_System_Project.cpp
@@ -0,0 +1,35 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+#include <tchar.h>
+//---------------------------------------------------------------------------
+USEFORM("Main_Unit.cpp", Main_form);
+//---------------------------------------------------------------------------
+int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
+{
+	try
+	{
+		Application->Initialize();
+		Application->MainFormOnTaskBar = true;
+		Application->CreateForm(__classid(TMain_form), &Main_form);
+		Application->Run();
+	}
+	catch (Exception &exception)
+	{
+		Application->ShowException(&exception);
+	}
+	catch (...)
+	{
+		try
+		{
+			throw Exception("");
+		}
+		catch (Exception &exception)
+		{
+			Application->ShowException(&exception);
+		}
+	}
+	return 0;
+}
+//---------------------------------------------------------------------------
---
+++ Main_Unit.h
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------
+
+#ifndef Main_UnitH
+#define Main_UnitH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+//---------------------------------------------------------------------------
+class TMain_form : public TForm
+{
+__published:	// IDE-managed Components
+private:	// User declarations
+public:		// User declarations
+	__fastcall TMain_form(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TMain_form *Main_form;
+//---------------------------------------------------------------------------
+#endif
